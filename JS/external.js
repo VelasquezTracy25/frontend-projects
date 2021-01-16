4330c667a9bca6af14797d1902d0740a47bcfde8
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-01-15 22:44:35 -0600

    Practice for alert, prompt, and confirm

---
+++ JS/external.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log('Hello from JavaScript!');
+
+alert("Welcome to my website!");
+
+var userInput = prompt('What\'s your favorite color?');
+console.log('The user entered: ' + userInput);
+
+// the following line will show the OK/CANCEL confirm dialog
+var confirmed = confirm('Exit?');
+console.log(confirmed); // will be either true or false
+
+
+
