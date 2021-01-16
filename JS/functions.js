d88f6000f5eb740e0aaeacc667518cb7a29a4337
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-01-15 22:58:48 -0600

    JS 101s up to 28

---
+++ JS/functions.js
@@ -0,0 +1,5 @@
+// Write a function definition named remainder that takes in two numbers and returns the remainder of first argument divided by the second argument.
+
+function remainder(num1, num2){
+    return num1 % num2;
+}
