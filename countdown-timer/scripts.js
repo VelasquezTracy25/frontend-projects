b457281597f7d76b09557f585795b56f76c620ae
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-01-18 16:55:32 -0600

    Added function to add zero if hours/minutes/seconds are less than 10

--- countdown-timer/scripts.js
+++ countdown-timer/scripts.js
@@ -17,11 +17,15 @@ function countdown() {
     const seconds = (Math.floor(totalSeconds % 60));
 
     daysElement.innerHTML = days;
-    hoursElement.innerHTML = hours;
-    minutesElement.innerHTML = minutes;
-    secondsElement.innerHTML = seconds;
+    hoursElement.innerHTML = formatTime(hours);
+    minutesElement.innerHTML = formatTime(minutes);
+    secondsElement.innerHTML = formatTime(seconds);
 
 console.log(days, hours, minutes, seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
 }
 
 //initial call
