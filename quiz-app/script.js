94217c9bc75306e3e3516aa3b06e52ea7722c064
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-01-18 22:53:21 -0600

    Styling for quiz app question box

---
+++ quiz-app/script.js
@@ -0,0 +1,31 @@
+const quizData = [
+{
+    question: 'How old is Tracy?',
+    a: '10',
+    b: '17',
+    c: '26',
+    d: '100',
+    correct: 'c'
+}, {
+    question: 'What is Tracy\'s favorite ice cream?',
+    a: 'Chocolate',
+    b: 'Vanilla',
+    c: 'Strawberry',
+    d: 'Banana',
+    correct: 'a'
+}, {
+    question: 'Where did Tracy study web development?',
+    a: 'UTSA',
+    b: 'Harvard',
+    c: 'Codeup',
+    d: 'YouTube',
+    correct: 'C'
+}, {
+    question: 'What is Tracy\'s favorite coding language?',
+    a: 'Java',
+    b: 'Python',
+    c: 'Javascript',
+    d: 'HTML',
+    correct: 'b'
+}
+]
